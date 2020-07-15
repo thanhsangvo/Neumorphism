ebbffe3f636a54c3e0d924eef339acd073c9de5b
thanhsangvo <pl.thanhsang0710@gmail.com>
2020-07-15 14:55:25 +0700

    Initial Commit

---
+++ Neumorphism/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Neumorphism
+//
+//  Created by Võ Thanh Sang on 7/15/20.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
